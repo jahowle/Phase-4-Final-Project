635ebb19e379b1603ccc5104a3453c8f6100ea6f
Jonny <jonny.howle@gmail.com>
2023-02-14 19:11:16 -0700

    needs list now returns Neighbor and Category info

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -4,7 +4,7 @@ class NeedsController < ApplicationController
 
     def index
         needs = Need.all
-        render json: needs
+        render json: needs, include: [:neighbor, :category]
     end
     
     def show
