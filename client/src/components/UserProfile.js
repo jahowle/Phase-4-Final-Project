a6201a2ae8dc9e6b93e09bf2657c3f44e8ca8dfd
Jonny <jonny.howle@gmail.com>
2023-02-22 08:58:22 -0700

    profile page renders list of needs for belonging to that user

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,14 +1,18 @@
 import React from "react";
+import NeedsList from "./NeedsList";
 
 
 function UserProfile({user, needs}) {
 
-    console.log(needs)
+    const needsToDisplay = needs.filter((need) => need.user_id === user.id)
+
+    console.log(needsToDisplay)
 
 
     return(
         <div>
             <h1>{user.username}'s Profile</h1>
+            <NeedsList needs={needsToDisplay}/>
         </div>
     )
 }
