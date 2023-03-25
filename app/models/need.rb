d184f543711f728ecd137f7b93cee0fb3afc8b87
Jonny <jonny.howle@gmail.com>
2023-03-25 13:57:06 -0600

    implemented custom validation to prevent blank donations

--- app/models/need.rb
+++ app/models/need.rb
@@ -5,16 +5,23 @@ class Need < ApplicationRecord
     has_many :donations
     has_many :donors, through: :donations
 
-    validates :description, :amount, :neighbor_id, :category_id, presence: true
+    validates :description, :amount, :neighbor_id, :category_id, :remaining_balance, presence: true
     validates :amount, numericality: {only_integer: true, less_than_or_equal_to: 400}
 
     validate :no_negative_balance
+    validate :no_blank_value
 
     def no_negative_balance
         if self.remaining_balance < 0
             errors.add(:amount, "too large")
         end
        
+    end
+
+    def no_blank_value
+        if self.remaining_balance == self.remaining_balance_was
+            errors.add(:amount, "can't be blank")
+        end
     end
 
 end
